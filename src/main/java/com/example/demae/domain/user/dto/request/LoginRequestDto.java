dd62b5a0f71917ce4fbba43f7c4a8c80d1bded5c
tls3254 <bikim9923@gmail.com>
2024-06-20 14:52:08 +0900

    refactoring : 위치 변경

--- src/main/java/com/example/demae/domain/user/dto/request/LoginRequestDto.java
+++ src/main/java/com/example/demae/domain/user/dto/request/LoginRequestDto.java
@@ -1,4 +1,4 @@
-package com.example.demae.domain.user.dto;
+package com.example.demae.domain.user.dto.request;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/com/example/demae/domain/user/dto/request/SignupRequestDto.java
+++ src/main/java/com/example/demae/domain/user/dto/request/SignupRequestDto.java
@@ -1,4 +1,4 @@
-package com.example.demae.domain.user.dto;
+package com.example.demae.domain.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
